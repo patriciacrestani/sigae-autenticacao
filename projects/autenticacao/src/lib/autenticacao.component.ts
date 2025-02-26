b01eda4eb85c902bfc91d507e66cc16c59a1f876
Patrícia Martins Rocca Crestani <patricia.crestani@viceri.com.br>
2025-02-26 15:19:08 -0300

    First commit

---
+++ projects/autenticacao/src/lib/autenticacao.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-autenticacao',
+  imports: [],
+  template: `
+    <p>
+      autenticacao works!
+    </p>
+  `,
+  styles: ``
+})
+export class AutenticacaoComponent {
+
+}
---
+++ projects/autenticacao/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of autenticacao
+ */
+
+export * from './lib/autenticacao.service';
+export * from './lib/autenticacao.component';
