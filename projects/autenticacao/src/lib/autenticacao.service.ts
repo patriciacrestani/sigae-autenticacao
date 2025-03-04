9b9923761185dce2364ae6a6688316350420f09b
Patrícia Martins Rocca Crestani <patricia.crestani@viceri.com.br>
2025-03-04 16:36:06 -0300

    feat: adiciona demais métodos de autenticação

--- projects/autenticacao/src/lib/autenticacao.service.ts
+++ projects/autenticacao/src/lib/autenticacao.service.ts
@@ -1,13 +1,28 @@
 
 import { Injectable } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular';
+import { AuthService, User } from '@auth0/auth0-angular';
 
 @Injectable()
 export class AutenticacaoService {
-
   constructor(private authService: AuthService) { }
 
-  public login(): void {
+  public loginWithRedirect(): void {
     this.authService.loginWithRedirect();
+  }
+
+  public loginWithPopup(): void {
+    this.authService.loginWithPopup();
+  }
+  
+  public logout(): void {
+    this.authService.logout();
+  }
+
+  public get user() {
+    return this.authService.user$;
+  }
+  
+  public get isAuthenticated() {
+    return this.authService.isAuthenticated$;
   }
 }
